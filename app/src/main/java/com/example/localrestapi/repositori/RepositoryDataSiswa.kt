362ee9fec5ae3067b8f62c41c3d5f56d1fdb6e7e
Arsyan <syandi.2901@gmail.com>
2025-12-18 01:26:41 -0800

    final code RepositoryDataSiswa.kt

--- app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
@@ -1,6 +1,8 @@
 package com.example.localrestapi.repositori
 
+import com.example.localrestapi.apiservice.ServiceApiSiswa
 import com.example.localrestapi.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
@@ -8,4 +10,17 @@ interface RepositoryDataSiswa{
  //   suspend fun getSatuSiswa(id: Int) : DataSiswa
    // suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
     //suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDatasiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+        .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
+    //override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa
+    //override suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    //override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceApiSiswa
 }
